8d445e397cb4b7535f3d9c0c8c46a2a4c3dcc5a3
kerby <lovincekerby99@gmail.com>
2022-02-19 16:44:50 -0300

    commit

--- my-app/src/User/HomePage.jsx
+++ my-app/src/User/HomePage.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 import "./style.css";
 
 const HomePage = () => {
+
+    const [email, seEmail]= useState("");
+    const [password, setPassword]= useState("");
+
+
+    const handleSubmit = (e) => {
+        e.priventDefault();
+        console.log("submit");
+    }
     return  (
         
         <div id="login">
             <h1 className="title">Login do Sistema</h1>
-            <form className="form">
+            <form className="form" onSubmit={handleSubmit}>
                 <div className="field">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" id="email" />
