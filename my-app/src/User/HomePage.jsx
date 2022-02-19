907d66eeaed5c8b9e690e66e27fcefd2ff173ee0
kerby <lovincekerby99@gmail.com>
2022-02-19 16:13:24 -0300

    commit

--- my-app/src/User/HomePage.jsx
+++ my-app/src/User/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./style.css";
 
 const HomePage = () => {
     return  (
@@ -10,6 +11,14 @@ const HomePage = () => {
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" id="email" />
 
+                </div>
+                <div className="field">
+                    <label htmlFor="password">Senha</label>
+                    <input type="password" name="password" id="password" />
+
+                </div>
+                <div className="actions">
+                    <button type="submit">Entrar</button>
 
                 </div>
 
