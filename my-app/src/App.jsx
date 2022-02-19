db9b1af8f3f245ef91aa48231d741d428939faa9
kerby <lovincekerby99@gmail.com>
2022-02-19 15:15:19 -0300

    commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Login from './User/Login/Login';
+import HomePage from './User/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+          <HomePage />
+          
+      
+      
+    </div>
+  );
+}
+
+export default App;
