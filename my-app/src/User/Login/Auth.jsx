f41b9f45611191d27135304567e5a951148ce0d7
kerby <lovincekerby99@gmail.com>
2022-02-26 15:29:23 -0300

    commit

--- my-app/src/User/Login/Auth.jsx
+++ my-app/src/User/Login/Auth.jsx
@@ -1,3 +1,40 @@
-import React, {createContext} from "react";
+import React, {useState, createContext} from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext(); 
+
+export const AuthProvider = ({children}) => {
+
+    const navigate = useNavigate();
+
+    const [user,setUser] = useState(null);
+
+    const Login = (email, password) => {
+        console.log("Login Auth" ,{ email, password});
+
+        if(password === "secret") { 
+            //função para verificar a senha e dirigir ao pagina navigação
+            setUser({ id:"123", email});
+            navigate("/Login");
+        }else{
+            console.log("Senha invalida")
+        }
+    };
+    const logout = () => {
+        console.log("logout");
+        setUser(null);
+        navigate("/");
+    };
+
+
+
+    return (
+        <AuthContext.Provider 
+        value={{authenticated: !! user,user,Login,logout}}>
+            {children}
+         </AuthContext.Provider>
+
+
+
+    );
+}
