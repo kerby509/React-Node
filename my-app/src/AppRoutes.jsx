8cd33f1dc7d485c5652161c79ce880b6b0bd84db
kerby <lovincekerby99@gmail.com>
2022-02-26 14:02:44 -0300

    authentificado o usuario

--- my-app/src/AppRoutes.jsx
+++ my-app/src/AppRoutes.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
     BrowserRouter as Router,
     Route,
@@ -8,18 +9,30 @@ import {
 
 import Login from './User/Login/Login';
 import HomePage from './User/HomePage';
+import { AuthContext } from "./User/Login/Auth";
 
 
 
 const AppRoutes = () => {
+    const [user,setUser] = useState(null);
+
+    const Login = (email, password) => {
+        console.log("Login Auth" ,{ email, password});
+        setUser({ id:"123", email});
+    };
+    const logout = () => {
+        console.log("logout");
+    };
     return(
         <Router>
+            <AuthContext.Provider value={{authenticated: !! user,user,Login,logout}}>
             <Routes>
                 <Route exact path="/login" element= {<Login />}/>
                 <Route exact path="/" element= {<HomePage />}/>
 
 
             </Routes>
+            </AuthContext.Provider>
         </Router>
     );
 };
