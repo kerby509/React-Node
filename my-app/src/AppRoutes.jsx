cf86adf84c41023776163e6e72273cf46b72af4a
kerby <lovincekerby99@gmail.com>
2022-02-26 15:29:15 -0300

    commit

--- my-app/src/AppRoutes.jsx
+++ my-app/src/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import {
     BrowserRouter as Router,
     Route,
@@ -9,30 +9,22 @@ import {
 
 import Login from './User/Login/Login';
 import HomePage from './User/HomePage';
-import { AuthContext } from "./User/Login/Auth";
+import { AuthContext, AuthProvider } from "./User/Login/Auth";
 
 
 
 const AppRoutes = () => {
-    const [user,setUser] = useState(null);
-
-    const Login = (email, password) => {
-        console.log("Login Auth" ,{ email, password});
-        setUser({ id:"123", email});
-    };
-    const logout = () => {
-        console.log("logout");
-    };
+   
     return(
         <Router>
-            <AuthContext.Provider value={{authenticated: !! user,user,Login,logout}}>
+            <AuthProvider>
             <Routes>
                 <Route exact path="/login" element= {<Login />}/>
                 <Route exact path="/" element= {<HomePage />}/>
 
 
             </Routes>
-            </AuthContext.Provider>
+            </AuthProvider>
         </Router>
     );
 };
