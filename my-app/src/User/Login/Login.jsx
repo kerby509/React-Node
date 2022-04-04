bf83eac4ca3a2fbeea97f1e4004f622feae08fd0
kerby <lovincekerby99@gmail.com>
2022-04-04 19:53:09 -0300

    commit

--- my-app/src/User/Login/Login.jsx
+++ my-app/src/User/Login/Login.jsx
@@ -3,7 +3,38 @@ import './Login.css';
 
 
 const Login = () => {
-    return  <div>pagina inicial</div>
+    return (
+    <div className="Login">
+        <img src={"./logo.jpeg"}/>
+            <div className="menu">
+
+                <div className="logo">
+                <h1>Condomino Residencial</h1>
+                    <h1>da Efapi Chapeco</h1>
+                   
+                    <div>
+                        <p className="par"> O Condomínio Residencial da Efapi está localizado a leste de Chapeco, no atual Setor 
+                 Habitacional São Bartolomeu. A Fazenda Taboquinha, de onde se originam as terras de nosso
+                  Condomínio, tem documentação centenária, remontando ao início do século XIX, quando as  transações
+                   imobiliárias eram anotadas pelas secretarias das igrejas católicas, por meio de  “registros paroquiais”.</p> 
+                    </div>
+                    <div>
+                        <p className="par"> Nosso Condomínio resultante do parcelamento da fazenda original,  denominado “Quinhão 11”, também conhecido por “Serrinha”, com a área de 218 hectares. A Fazenda
+                     Taboquinha, a partir de 1955, pertenceu a Pedro Ferreira Alves e Dona Romualda,  ocupando uma área de 1700 alqueires, abrangendo
+                      um território que tinha divisas, em termos contemporâneos, próximas à Ermida Dom Bosco, à QI-19 e ao Presídio da Papuda. Seus limites faziam
+                      referência ao Córrego da Divisa, à Fazenda Paranoá, ao Córrego Cabeça de Veado, ao Córrego Mata Grande, ao Córrego Santo Antônio da Papuda, ao Córrego
+                      Taboca e ao Rio São Bartolomeu. Durante o processo de instalação da capital Federal, uma parte 
+                      da Fazenda Taboquinha foi negociada com o Governo, enquanto que vários quinhões foram mantidos,
+                       integralmente, sob o domínio e posse de seus proprietários.</p>
+                    </div>
+
+
+                   
+                
+            </div>
+        </div>
+    </div>
+    )
      
     
 }
