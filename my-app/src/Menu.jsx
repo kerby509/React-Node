c4167aef4454315419ae17300bbaa431609118aa
kerby <lovincekerby99@gmail.com>
2022-04-04 20:32:42 -0300

    commit

---
+++ my-app/src/Menu.jsx
@@ -0,0 +1,26 @@
+import react from 'react';
+
+function Menu(){
+    return(
+        <div>
+            <div className='Menu'>
+                <h2>
+                <a href=''>Morador</a>
+                <a href=''>Lista morador</a>
+                <a href=''>Agendar</a>
+                <a href=''>Reservação</a>
+                <a href=''>Morador</a>
+                </h2>
+
+
+
+
+            </div>
+        </div>
+
+    );
+}
+
+
+
+export default Menu;
